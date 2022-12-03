8c1666d65d286b4c7bb2fab409988d2a12fe042d
Alexey Filippov <sobsgm@gmail.com>
2022-12-03 16:23:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 chmod o-r /
 apt update && apt upgrade --yes
 apt install htop mc nginx --yes
-apt install php8.1-fpm php8.1-mbstring php8.1-json php8.1-zip --yes
+apt install php8.1-fpm php8.1-mbstring php8.1-zip --yes
