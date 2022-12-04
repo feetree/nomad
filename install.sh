516aa100325309f676313b1b8916e85fb799cbaa
Alexey Filippov <sobsgm@gmail.com>
2022-12-04 06:52:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 chmod o-r /
 apt update && apt upgrade --yes
+
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
 apt install htop mc nginx --yes
 apt install php8.1-fpm php8.1-mbstring php8.1-dom php8.1-zip php8.1-mysql php8.1-simplexml php8.1-xml php8.1-gd php8.1-curl --yes
